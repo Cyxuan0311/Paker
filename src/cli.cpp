200591d4c7296ecd0667db9fc661692bfa8f374a
cyxyucheng <cyxyucheng@tencent.com>
2025-07-18 15:33:31 +0800

    Add CLI commands for enhanced package management: Introduced subcommands for adding dependencies, setting project description and version, recursive addition, displaying dependency tree, managing lock files, upgrading packages, searching for available packages, showing package info, updating local packages, and cleaning unused packages.

--- src/cli.cpp
+++ src/cli.cpp
@@ -14,10 +14,26 @@ int run_cli(int argc, char* argv[]) {
 
     // add
     std::string add_pkg;
-    auto add = app.add_subcommand("add", "Add a dependency");
-    add->add_option("package", add_pkg, "Package name to add")->required();
+    auto add = app.add_subcommand("add", "Add a dependency or project info");
+    add->add_option("package", add_pkg, "Package name to add");
     add->callback([&]() {
-        pm_add(add_pkg);
+        if (!add_pkg.empty()) pm_add(add_pkg);
+    });
+
+    // add desc
+    std::string desc;
+    auto add_desc = add->add_subcommand("desc", "Set project description");
+    add_desc->add_option("desc", desc, "Project description")->required();
+    add_desc->callback([&]() {
+        pm_add_desc(desc);
+    });
+
+    // add vers
+    std::string vers;
+    auto add_vers = add->add_subcommand("vers", "Set project version");
+    add_vers->add_option("vers", vers, "Project version")->required();
+    add_vers->callback([&]() {
+        pm_add_version(vers);
     });
 
     // remove
@@ -32,6 +48,68 @@ int run_cli(int argc, char* argv[]) {
     auto list = app.add_subcommand("list", "List dependencies");
     list->callback([]() {
         pm_list();
+    });
+
+    // add-recursive
+    std::string add_rec_pkg;
+    auto add_rec = app.add_subcommand("add-recursive", "Recursively add a dependency and its dependencies");
+    add_rec->add_option("package", add_rec_pkg, "Package name to add recursively")->required();
+    add_rec->callback([&]() {
+        pm_add_recursive(add_rec_pkg);
+    });
+
+    // tree
+    auto tree = app.add_subcommand("tree", "Show dependency tree");
+    tree->callback([]() {
+        pm_tree();
+    });
+
+    // lock
+    auto lock = app.add_subcommand("lock", "Generate or update Paker.lock file");
+    lock->callback([]() {
+        pm_lock();
+    });
+
+    // install-lock
+    auto install_lock = app.add_subcommand("install-lock", "Install dependencies from Paker.lock");
+    install_lock->callback([]() {
+        pm_install_lock();
+    });
+
+    // upgrade
+    std::string upgrade_pkg;
+    auto upgrade = app.add_subcommand("upgrade", "Upgrade all dependencies or a specific dependency");
+    upgrade->add_option("package", upgrade_pkg, "Package name to upgrade (optional)");
+    upgrade->callback([&]() {
+        pm_upgrade(upgrade_pkg);
+    });
+
+    // search
+    std::string search_kw;
+    auto search = app.add_subcommand("search", "Search available packages");
+    search->add_option("keyword", search_kw, "Keyword to search")->required();
+    search->callback([&]() {
+        pm_search(search_kw);
+    });
+
+    // info
+    std::string info_pkg;
+    auto info = app.add_subcommand("info", "Show package info");
+    info->add_option("package", info_pkg, "Package name")->required();
+    info->callback([&]() {
+        pm_info(info_pkg);
+    });
+
+    // update
+    auto update = app.add_subcommand("update", "Update all local packages");
+    update->callback([]() {
+        pm_update();
+    });
+
+    // clean
+    auto clean = app.add_subcommand("clean", "Clean unused or broken packages");
+    clean->callback([]() {
+        pm_clean();
     });
 
     CLI11_PARSE(app, argc, argv);
